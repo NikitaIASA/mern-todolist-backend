4e9e06b5223fb58588df7a05c1cc00db69a8a75b
Nikita Chaika <iasastudent2021@gmail.com>
2023-01-10 02:20:31 +0200

    handleValidationErrors

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 
 import { registerValidation, loginValidation, taskCreateValidation } from './validations.js';
 
-import {checkAuth } from './utils/index.js';
+import {checkAuth, handleValidationErrors} from './utils/index.js';
 
 
 import { UserController, TaskController } from './controllers/index.js';
@@ -22,20 +22,21 @@ app.use(express.json());
 
 // AUTH
 
-app.post('/auth/login', loginValidation, UserController.login);
-app.post('/auth/register', registerValidation, UserController.register);
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/register', registerValidation,handleValidationErrors,  UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
 // CRUD
 
 app.get('/tasks', TaskController.getAllTaks);
 app.get('/tasks/:id', TaskController.getOneTask);
-app.post('/tasks', checkAuth, taskCreateValidation, TaskController.createTask);
+app.post('/tasks', checkAuth, taskCreateValidation, handleValidationErrors, TaskController.createTask);
 app.delete('/tasks/:id', checkAuth, TaskController.removeTask);
 app.patch(
     '/tasks/:id',
     checkAuth,
     taskCreateValidation,
+    handleValidationErrors,
     TaskController.updateTask,
   );
 
